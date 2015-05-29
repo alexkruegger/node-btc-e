b63c81f6025a2a179acf2028a4cc012056b5f55e
alexkruegger <krumobile@gmail.com>
2015-05-29 16:43:23 +0300

    Update btc-e.js

--- btc-e.js
+++ btc-e.js
@@ -1,19 +1,20 @@
 'use strict';
 var request = require('request'),
-    crypto = require('crypto'),
-    querystring = require('querystring');
+  crypto = require('crypto'),
+  querystring = require('querystring');
 
-var BTCE = function(apiKey, secret, options) {
+var BTCE = function (apiKey, secret, options) {
   this.url = 'https://btc-e.com/tapi';
   this.publicApiUrl = 'https://btc-e.com/api/3/';
-  this.timeout = 20000;
+  this.timeout = 5000;
   this.apiKey = apiKey;
   this.secret = secret;
-  this._strictSSL = true;
+  this._strictSSL = false;
 
   if (typeof options === 'function') {
     this.nonce = options;
-  } else if (options) {
+  }
+  else if (options) {
     this.nonce = options.nonce;
     this.agent = options.agent;
 
@@ -44,19 +45,20 @@ BTCE.prototype._sendRequest = function (options, callback) {
     requestOptions[key] = options[key];
   }
 
-  request(requestOptions, function(err, response, body) {
-    if(err || response.statusCode !== 200) {
+  request(requestOptions, function (err, response, body) {
+    if (err || response.statusCode !== 200) {
       return callback(new Error(err || response.statusCode));
     }
 
     var result;
     try {
       result = JSON.parse(body);
-    } catch(error) {
-      return callback(new Error(error));
+    }
+    catch (error) {
+      return callback(error);
     }
 
-    if(result.error) {
+    if (result.error) {
       return callback(new Error(result.error));
     }
 
@@ -64,17 +66,18 @@ BTCE.prototype._sendRequest = function (options, callback) {
   });
 };
 
-BTCE.prototype.makeRequest = function(method, params, callback) {
+BTCE.prototype.makeRequest = function (method, params, callback) {
   var self = this;
 
-  if(!self.apiKey || !self.secret) {
+  if (!self.apiKey || !self.secret) {
     return callback(new Error('Must provide API key and secret to use the trade API.'));
   }
 
   // If the user provided a function for generating the nonce, then use it.
-  if(self.nonce) {
+  if (self.nonce) {
     params.nonce = self.nonce();
-  } else {
+  }
+  else {
     params.nonce = Math.round((new Date()).getTime() / 1000);
   }
 
@@ -98,45 +101,42 @@ BTCE.prototype.makeRequest = function(method, params, callback) {
   }, callback);
 };
 
-BTCE.prototype.makePublicApiRequest = function(pair, method, callback) {
-  this._sendRequest({
-    url: this.publicApiUrl + method + '/' + pair
-  }, callback);
-};
-
-BTCE.prototype.getInfo = function(callback) {
+BTCE.prototype.getUserInfo = function (callback) {
   this.makeRequest('getInfo', {}, callback);
 };
 
-BTCE.prototype.transHistory = function(params, callback) {
+BTCE.prototype.transHistory = function (params, callback) {
   this.makeRequest('TransHistory', params, callback);
 };
 
-BTCE.prototype.tradeHistory = function(params, callback) {
+BTCE.prototype.tradeHistory = function (params, callback) {
   this.makeRequest('TradeHistory', params, callback);
 };
 
-BTCE.prototype.orderInfo = function(paramsOrOrderId, callback) {
+BTCE.prototype.orderInfo = function (paramsOrOrderId, callback) {
   var inputType = typeof paramsOrOrderId;
-  var input = (inputType === 'string' || inputType === 'number') ?
-    {order_id: paramsOrOrderId} : paramsOrOrderId;
+  var input = (inputType === 'string' || inputType === 'number') ? {
+    order_id: paramsOrOrderId
+  } : paramsOrOrderId;
   this.makeRequest('OrderInfo', input, callback);
 };
 
-BTCE.prototype.orderList = function(params, callback) {
+BTCE.prototype.orderList = function (params, callback) {
   this.makeRequest('OrderList', params, callback);
 };
 
-BTCE.prototype.activeOrders = function(pair, callback) {
+BTCE.prototype.activeOrders = function (pair, callback) {
   if (!callback) {
     callback = pair;
     pair = null;
   }
 
-  this.makeRequest('ActiveOrders', {pair: pair}, callback);
+  this.makeRequest('ActiveOrders', {
+    pair: pair
+  }, callback);
 };
 
-BTCE.prototype.trade = function(pair, type, rate, amount, callback) {
+BTCE.prototype.trade = function (pair, type, rate, amount, callback) {
   this.makeRequest('Trade', {
     'pair': pair,
     'type': type,
@@ -145,27 +145,49 @@ BTCE.prototype.trade = function(pair, type, rate, amount, callback) {
   }, callback);
 };
 
-BTCE.prototype.cancelOrder = function(paramsOrOrderId, callback) {
+BTCE.prototype.cancelOrder = function (paramsOrOrderId, callback) {
   var inputType = typeof paramsOrOrderId;
-  var input = (inputType === 'string' || inputType === 'number') ?
-    {order_id: paramsOrOrderId} : paramsOrOrderId;
+  var input = (inputType === 'string' || inputType === 'number') ? {
+    order_id: paramsOrOrderId
+  } : paramsOrOrderId;
   this.makeRequest('CancelOrder', input, callback);
 };
 
-BTCE.prototype.ticker = function(pair, callback) {
-  this.makePublicApiRequest(pair, 'ticker', callback);
+
+// ***************** PUBLIC *************************************
+BTCE.prototype.makePublicApiRequest = function (pair, method, options, callback) {
+
+  var url = this.publicApiUrl + method + '/' + pair;
+
+  url += '?ignore_invalid=1';
+
+  for (var k in options) {
+    url += '&' + k + '=' + options[k];
+  }
+
+  this._sendRequest({
+    url: url
+  }, callback);
 };
 
-BTCE.prototype.trades = function(pair, callback) {
-  this.makePublicApiRequest(pair, 'trades', callback);
+BTCE.prototype.ticker = function (pair, callback) {
+  this.makePublicApiRequest(pair, 'ticker', {}, callback);
 };
 
-BTCE.prototype.depth = function(pair, callback) {
-  this.makePublicApiRequest(pair, 'depth', callback);
+BTCE.prototype.trades = function (pair, options, callback) {
+  this.makePublicApiRequest(pair, 'trades', options, callback);
 };
 
-BTCE.prototype.fee = function(pair, callback) {
-  this.makePublicApiRequest(pair, 'fee', callback);
+BTCE.prototype.depth = function (pair, options, callback) {
+  this.makePublicApiRequest(pair, 'depth', options, callback);
+};
+
+BTCE.prototype.fee = function (pair, callback) {
+  this.makePublicApiRequest(pair, 'fee', {}, callback);
+};
+
+BTCE.prototype.info = function (pair, callback) {
+  this.makePublicApiRequest(pair, 'info', {}, callback);
 };
 
 module.exports = BTCE;
